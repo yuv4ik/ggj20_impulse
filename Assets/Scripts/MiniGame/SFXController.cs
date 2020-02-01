8bb0e491cc8aabc15c3099f8621dcca071f2d0b0
yuv4ik <yuv4k@gmail.com>
2020-02-01 21:40:28 +0200

    mini game sfx & bug fix

---
+++ Assets/Scripts/MiniGame/SFXController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SFXController : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip _sfxCollect;
+
+    AudioSource _audioSource;
+
+    void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayCollectSFX() => _audioSource.PlayOneShot(_sfxCollect, 1f);
+}
