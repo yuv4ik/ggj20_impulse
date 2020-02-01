abd6bea33575242709c2e687e055df0dd7d521fe
yuv4ik <yuv4k@gmail.com>
2020-02-01 22:09:25 +0200

    show E o user interectables

--- Assets/Scripts/CharacterCollisionController.cs
+++ Assets/Scripts/CharacterCollisionController.cs
@@ -2,6 +2,8 @@
 
 public class CharacterCollisionController : MonoBehaviour
 {
+    bool _showHint;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("WrongDecision"))
@@ -12,9 +14,28 @@ public class CharacterCollisionController : MonoBehaviour
 
     void OnCollisionStay(Collision other)
     {
-        if (other.gameObject.CompareTag("PlayerInteractable") && Input.GetKey(KeyCode.E))
+        if (other.gameObject.CompareTag("PlayerInteractable"))
         {
-            other.gameObject.GetComponent<IPlayerInteractable>().Interact();
+            _showHint = true;
+            if (Input.GetKey(KeyCode.E))
+            {
+                other.gameObject.GetComponent<IPlayerInteractable>().Interact();
+            }
         }
+        else
+        {
+            _showHint = false;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (!_showHint) return;
+
+        var _hintStyle = GUI.skin.GetStyle("Label");
+        _hintStyle.alignment = TextAnchor.UpperCenter;
+        _hintStyle.fontStyle = FontStyle.Bold;
+        _hintStyle.fontSize = 50;
+        GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 25, 100, 50), "E", _hintStyle);
     }
 }
